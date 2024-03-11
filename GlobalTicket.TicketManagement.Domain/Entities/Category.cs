65202b0fa6f0c2080bd42ac1ad1cb13368eda355
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-03-11 17:26:16 +0000

    Added the folder layout for the Clean Architecture and Domain Entities

---
+++ GlobalTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using GlobalTicket.TicketManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Event>? Events { get; set; }
+    }
+}
